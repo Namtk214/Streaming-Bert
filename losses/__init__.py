5f63d10557c54f5b404927df966423664b93547c
Namtk214 <nam.ngothanh05@gmail.com>
2026-04-23 10:12:35 +0700

    delete

--- losses/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Loss functions cho Baseline2."""
-
-from .weighted_prefix_loss import weighted_cumulative_loss
-
-__all__ = ["weighted_cumulative_loss"]
--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Models cho Baseline2: Early-Exit with Weighted Loss."""
-
-from .turn_encoder import TurnEncoder
-from .cross_turn_attention import CrossTurnAttention
-from .early_exit_model import EarlyExitWeightedModel
-
-__all__ = ["TurnEncoder", "CrossTurnAttention", "EarlyExitWeightedModel"]
